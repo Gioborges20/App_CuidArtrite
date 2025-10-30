c4e1893852c969efd53a5b40aa3148b066e5885d
Giovana Borges <Giovanadacborges.gdcb@gmail.com>
2025-10-30 17:50:55 -0300

    Adicionado flutter

---
+++ app_cuidArtrite/README.md
@@ -0,0 +1,3 @@
+# app_osteoartrite
+
+A new Flutter project.
---
+++ app_cuidArtrite/android/app/src/main/kotlin/com/example/app_osteoartrite/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_osteoartrite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
