46f040230e2ffc5eae1496f5f4fc52bb8efa3341
hannarecks <hannareck07@gmail.com>
2025-11-20 18:28:58 -0300

    preparando o firebase

--- app_cuidArtrite/lib/main.dart
+++ app_cuidArtrite/lib/main.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'app_module.dart';
 import 'app_widget.dart';
+import 'package:firebase_core/firebase_core.dart'; //firebase
+import 'modules/services/firebase_options.dart'; //firebase
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); 
-
+  await Firebase.initializeApp( // Add inicialização do Firebase
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ModularApp(
     module: AppModule(),
     child: const AppWidget(),
