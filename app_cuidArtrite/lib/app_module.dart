f3a3653d423645422b48005316485981962ba3ca
hannarecks <hannareck07@gmail.com>
2025-11-20 21:54:34 -0300

    modulo pricipal é a homepage

--- app_cuidArtrite/lib/app_module.dart
+++ app_cuidArtrite/lib/app_module.dart
@@ -1,3 +1,4 @@
+import 'package:app_osteoartrite/modules/home/tela_principal_module.dart';
 import 'package:app_osteoartrite/modules/login/login_module.dart';
 import 'package:app_osteoartrite/modules/cadastro/cadastro_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -5,7 +6,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 class AppModule extends Module {
   @override
   void routes(RouteManager r) {
-    r.module('/', module: LoginModule());
+    r.module('/', module: TelaPrincipalModule());
     r.module('/Login', module: LoginModule());
     r.module('/cadastro', module: CadastroModule());
   }
