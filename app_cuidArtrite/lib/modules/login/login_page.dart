96070ecf387d28ed0287e8dea5c3b37c2d0b0d93
hannarecks <hannareck07@gmail.com>
2025-12-01 13:14:41 -0300

    verificações e validações do cadastro e login. Auth atualizado para ser singleton)

--- app_cuidArtrite/lib/modules/login/login_page.dart
+++ app_cuidArtrite/lib/modules/login/login_page.dart
@@ -2,6 +2,9 @@ import 'package:app_osteoartrite/modules/cadastro/cadastro_page.dart';
 import 'package:app_osteoartrite/modules/home/tela_principal_page.dart' show TelaPrincipalPage;
 import 'package:flutter/material.dart';
 import 'package:app_osteoartrite/shared/services/auth_service.dart';
+import 'package:show_hide_password/show_hide_password.dart';
+import 'dart:developer';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -65,6 +68,7 @@ class _LoginPageState extends State<LoginPage> {
                     child: Text('E-mail', style: TextStyle(fontSize: 18)),
                   ),
                   TextField(
+                    controller: emailController,
                     decoration: InputDecoration(
                       hintText: 'Digite seu e-mail',
                       border: OutlineInputBorder(),
@@ -75,27 +79,68 @@ class _LoginPageState extends State<LoginPage> {
                     padding: EdgeInsetsGeometry.all(5),
                     child: Text('Senha', style: TextStyle(fontSize: 18)),
                   ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Digite sua senha',
-                      border: OutlineInputBorder(),
-                    ),
+                  ShowHidePassword(
+                    passwordField: (bool hidePassword) {
+                      return TextField(
+                        controller: senhaController,
+                        obscureText: hidePassword,
+                        decoration: InputDecoration(
+                          hintText: 'Digite sua senha',
+                          border: OutlineInputBorder(),
+                        ),
+                      );
+                    },
                   ),
+
                   const SizedBox(height: 20),
+
                   ElevatedButton(
                     onPressed: () async {
                       try {
-                        await AuthService().login(
-                          emailController.text.trim(),
-                          senhaController.text.trim(),
-                        );
-                      } catch (e) {
-                        print("Erro ao fazer login: $e");   // Colocar logs usando dependencies
+                        if (emailController.text.isEmpty) {
+                          if (!mounted) return;
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('E-mail não pode ser vazio')),
+                          );
+                          return; // interrompe fluxo
+                        }
+                        if (senhaController.text.isEmpty) {
+                          if (!mounted) return;
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Senha não pode ser vazio')),
+                          );
+                          return; // interrompe fluxo
+                        }
+
+                        try {
+                          await AuthService.instance.login(
+                            emailController.text.trim(),
+                            senhaController.text.trim(),
+                          );
+
+                          if (!mounted) return;
+
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => TelaPrincipalPage()),
+                          );
+
+                        } on FirebaseAuthException catch (e) {
+                          if (!mounted) return;
+
+                          if (e.code == 'user-not-found' || e.code == 'wrong-password') {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('Usuário não encontrado ou senha incorreta')),
+                            );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('Erro ao fazer login: ${e.code}')),
+                            );
+                          }
+                        }
+                      } catch (e, stackTrace) {
+                        log("Erro ao fazer login: $e", name: "LoginPage", error: e, stackTrace: stackTrace);
                       }
-                      Navigator.push(
-                        context,
-                         MaterialPageRoute(builder: (context) => TelaPrincipalPage()),
-                        );
                     },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Color(0xFF13574C), // Cor verde do AppBar
@@ -132,6 +177,7 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
+  @override
   void dispose() {
     emailController.dispose();
     senhaController.dispose();
